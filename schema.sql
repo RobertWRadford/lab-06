0c18f64ed5dd13da26b0520f1e5bf8c30ee7fd74
RobertWRadford <robertr928@gmail.com>
2020-10-14 21:14:41 -0400

    add table data

--- schema.sql
+++ schema.sql
@@ -7,3 +7,27 @@ CREATE TABLE locations (
     latitude NUMERIC(10, 7),
     longitude NUMERIC(10, 7)
   );
+
+DROP TABLE IF EXISTS forecasts;
+
+CREATE TABLE forecasts (
+    id SERIAL PRIMARY KEY,
+    forecast VARCHAR(255),
+    time VARCHAR(255)
+  );
+
+DROP TABLE IF EXISTS trails;
+
+CREATE TABLE trails (
+    id SERIAL PRIMARY KEY,
+    trail_url VARCHAR(255),
+	name VARCHAR(255),
+ 	location VARCHAR(255),
+	length NUMERIC(10, 7),
+	condition_date VARCHAR(255),
+	condition_time VARCHAR(255),
+	conditions VARCHAR(255),
+	stars NUMERIC(10, 7),
+	star_votes NUMERIC(10, 7),
+	summary VARCHAR(255)
+	);
